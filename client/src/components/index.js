97aaaa75598b582f7d3a10b9086ae37c95b46dfa
Eric Bukowski <john.eric.bukowski@gmail.com>
2017-06-23 14:41:23 -0600

    Add 'Error' component to display error messages

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as App } from './App';
 export { default as Button } from './Button';
+export { default as Error } from './Error';
 export { default as Rates } from './Rates';
 export { default as Root } from './Root';
