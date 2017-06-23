4212037a96664c6113d8c05f330f5aa2dab6aaa8
Eric Bukowski <john.eric.bukowski@gmail.com>
2017-06-23 15:23:06 -0600

    Utilize new 'prop-types' module

--- client/src/components/Rates/index.js
+++ client/src/components/Rates/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Button, Error } from '../';
 
 const Rates = ({ isLoading, errorMessage, bittrex, btcE, poloniex, fetchExchangeRates }) => (
