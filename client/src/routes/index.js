71b3227f865d5413b9c2ad6bd224b0a89fe21a53
Eric Bukowski <john.eric.bukowski@gmail.com>
2017-06-23 08:16:15 -0600

    Add 'Rates' component skeleton
    This component will be used as the main display of exchange rate data.

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,15 @@
-import { App } from '../components';
+import { App, Rates } from '../components';
 
 export default {
   path: '/',
   component: App,
+  indexRoute: {
+    onEnter: (nextState, replace) => replace('/exchange-rates'),
+  },
+  childRoutes: [
+    {
+      path: 'exchange-rates',
+      component: Rates,
+    },
+  ],
 };
