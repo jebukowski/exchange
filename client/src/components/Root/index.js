c7052aee10e71b89820ca8063d070f08de7870ef
Eric Bukowski <john.eric.bukowski@gmail.com>
2017-06-23 08:35:28 -0600

    Add redux store with redux-saga middleware

--- client/src/components/Root/index.js
+++ client/src/components/Root/index.js
@@ -2,13 +2,14 @@ import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 
-const Root = ({ routes }) => (
-  <Provider>
+const Root = ({ store, routes }) => (
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
 );
 
 Root.propTypes = {
+  store: PropTypes.object,
   routes: PropTypes.object,
 };
 
